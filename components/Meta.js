6c22b18753e762c8fffc7bfa9c578d583000edd2
CubingSoda <cubingsoda@gmail.com>
2022-02-01 20:28:45 -0600

    better meta, icons with different sizes

--- components/Meta.js
+++ components/Meta.js
@@ -11,17 +11,14 @@ export default function Meta({ page }) {
 
       <meta
         name="keywords"
-        content="programming, code, coding, js, javascript, py, python, react, reactjs, cubingsoda"
+        content="cubingsoda, programming, code, coding, js, javascript, py, python, react, reactjs"
       />
       <meta
         name="description"
-        content="My personal website. I showcase my projects and I make posts every once in awhile"
+        content="CubingSoda | I showcase my projects and make posts every once in awhile. Reddit, Discord, and GitHub links are at the homepage."
       />
 
-      <meta
-        property="og:image"
-        content="https://cubingsoda.pages.dev/icon.png"
-      />
+      <meta property="og:image" content="/icon.png" />
       <meta property="og:title" content={title} />
       <meta
         property="og:description"
@@ -30,11 +27,31 @@ export default function Meta({ page }) {
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://cubingsoda.pages.dev" />
 
-      <link rel="icon" href="/icon.png" />
-      <link rel="shortcut icon" href="/icon.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/icon.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/icon.png" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicons/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicons/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicons/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link
+        rel="mask-icon"
+        href="/favicons/safari-pinned-tab.svg"
+        color="#6eb0ff"
+      />
+      <meta name="msapplication-TileColor" content="#6eb0ff" />
+      <meta name="theme-color" content="#6eb0ff" />
 
       <title>{title}</title>
     </Head>
