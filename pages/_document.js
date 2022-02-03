ac4a018e059393b119bc304824eafc4d6a80194b
CubingSoda <cubingsoda@gmail.com>
2022-02-02 22:37:25 -0600

    use link tag instead of @import for fonts

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,15 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Poppins&family=Roboto&display=swap"
+            rel="stylesheet"
+          ></link>
+        </Head>
+
         <body>
           <Main />
           <NextScript />
